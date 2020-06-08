5691b58bba5c44b4d762c08beb68ffa4ca80d535
Kuari <kuari@justmylife.cc>
2020-06-08 23:00:38 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kuari on 2020/6/8.
+//  Copyright © 2020 Kuari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
